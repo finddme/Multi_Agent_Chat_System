a6d6321de1c7f54c059296752ae5698b96bf1ffb
finddme <53667002+finddme@users.noreply.github.com>
2024-07-15 17:58:55 +0900

    Update streamlit_fastapi.py

--- streamlit_fastapi.py
+++ streamlit_fastapi.py
@@ -65,7 +65,7 @@ def run_convo():
     """, unsafe_allow_html=True)
     st.markdown("""<p class='custom-font'>😎 RAG 기반 블로그 검색창</p>""", unsafe_allow_html=True )
 
-    st.markdown("""3 pipeline (1)LLM/NLP/AI->블로그 포스트 RAG (2)일반 질문->Web search RAG (3)일상 대화->일반 chatbot</p>""", 
+    st.markdown("""<p class='custom-font2'>3 pipeline (1)LLM/NLP/AI->블로그 포스트 RAG (2)일반 질문->Web search RAG (3)일상 대화->일반 chatbot</p>""", 
                 unsafe_allow_html=True 
                 )
     # st.markdown(""":orange[*Query Router | Hallucination Grader | Reranker | Web search*<br><p class='small-font'>인공지능/LLM/NLP와 관련된 질문은 본 <strong>블로그 포스트</strong>를 기반으로 답변하고, 
