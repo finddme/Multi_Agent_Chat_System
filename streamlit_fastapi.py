f0d0e62a8f37ecdcd178bcf91a1d0ffd0902d50c
finddme <53667002+finddme@users.noreply.github.com>
2024-07-15 17:34:45 +0900

    Update streamlit_fastapi.py

--- streamlit_fastapi.py
+++ streamlit_fastapi.py
@@ -51,7 +51,7 @@ def run_convo():
     }
     </style>
     """, unsafe_allow_html=True)
-    st.markdown(""":orange[*Query Router | Hallucination Grader | Reranker | Web search*]""", 
+    st.markdown(""":orange[*Query Router | Hallucination Grader | Reranker | Web search*]:gray[검색어에 따라 3 가지 pipeline으로 나뉘어 결과를 생성합니다. (1)LLM/NLP/AI->블로그 포스트 RAG (2)일반 질문->Web search RAG (3)일상 대화->일반 chatbot]""", 
                 unsafe_allow_html=True 
                 )
     # st.markdown(""":orange[*Query Router | Hallucination Grader | Reranker | Web search*<br><p class='small-font'>인공지능/LLM/NLP와 관련된 질문은 본 <strong>블로그 포스트</strong>를 기반으로 답변하고, 
@@ -59,7 +59,7 @@ def run_convo():
     #             정보를 요구하는 질문이 아닐 경우 <strong>일상 대화 chatbot 버전</strong>으로 사용할 수 있습니다.</p>]""", 
     #             unsafe_allow_html=True 
     #             )
-    user_input = st.text_input('검색어에 따라 3 가지 pipeline으로 나뉘어 결과를 생성합니다. (1)LLM/NLP/AI->블로그 포스트 RAG (2)일반 질문->Web search RAG (3)일상 대화->일반 chatbot')
+    user_input = st.text_input('검색어를 입력하세요😎')
     # submitted = st.form_submit_button('Send')
     
     if user_input:
