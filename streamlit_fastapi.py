208ce63862e2539ad15f021951072e3911eaa12f
finddme <53667002+finddme@users.noreply.github.com>
2024-07-16 10:05:18 +0900

    Update streamlit_fastapi.py

--- streamlit_fastapi.py
+++ streamlit_fastapi.py
@@ -63,12 +63,12 @@ def run_convo():
     }
     </style>
     """, unsafe_allow_html=True)
-    st.markdown("""<p class='custom-font'>😎 RAG 기반 블로그 검색창</p>""", unsafe_allow_html=True )
+    # st.markdown("""<p class='custom-font'>😎 RAG 기반 블로그 검색창</p>""", unsafe_allow_html=True )
 
     # st.markdown("""<p class='custom-font2'>3 pipeline (1)LLM/NLP/AI->블로그 포스트 RAG (2)일반 질문->Web search RAG (3)일상 대화->일반 chatbot</p>""", 
     #             unsafe_allow_html=True 
     #             )
-    user_input = st.text_input('검색어를 입력하세요.')
+    user_input = st.text_input('😎 RAG 기반 블로그 검색창 | 검색어를 입력하세요.')
     # submitted = st.form_submit_button('Send')
     
     if user_input:
