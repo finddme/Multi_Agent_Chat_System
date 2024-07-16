6bb199d3e4b84e8e1b1cb41edb97abec2f5b0b5f
finddme <53667002+finddme@users.noreply.github.com>
2024-07-16 10:25:02 +0900

    Update streamlit_fastapi.py

--- streamlit_fastapi.py
+++ streamlit_fastapi.py
@@ -69,6 +69,7 @@ def run_convo():
     #             unsafe_allow_html=True 
     #             )
     user_input = st.text_input("""😎 RAG 기반 블로그 검색창 | 검색어를 입력하세요.
+    
                                 :orange[*Query Router | Hallucination Grader | Reranker | Web search*]""")
     # submitted = st.form_submit_button('Send')
     
