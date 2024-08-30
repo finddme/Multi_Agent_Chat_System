f5d1689781189c4cb0378d94dec4333acc93ab81
finddme <53667002+finddme@users.noreply.github.com>
2024-08-30 14:07:12 +0900

    Update streamlit.py

--- blog_search_box/streamlit.py
+++ blog_search_box/streamlit.py
@@ -29,7 +29,7 @@ async def run_convo():
     }
     </style>
     """, unsafe_allow_html=True)
-    st.markdown("""검색어를 입력하세요.\\
+    st.markdown("""**검색어를 입력하세요.**\\
                 :orange[**Law | Ai | conversation | web search | realtime | image generation**]""", 
                 unsafe_allow_html=True 
                 )
